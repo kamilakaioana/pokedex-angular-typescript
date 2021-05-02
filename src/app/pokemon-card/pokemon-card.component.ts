a00feeafaee39d848d686a87081d574ea2dd6c73
Kamila Kaioana <kamilaaleixocosta@hotmail.com>
2021-05-02 20:41:59 -0300

    service working with api

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,22 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Pokemon } from 'src/_model/Pokemon';
+import { Component, Input } from '@angular/core';
+import { getPokemonImage, getPokemonNumber, Pokemon } from 'src/_model/Pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
+
 export class PokemonCardComponent {
   @Input()
   public pokemon: Pokemon;
 
-  public leadingZero(str: string | number, size = 3): string {
-    let s = String(str);
+  public getPokemonImage = getPokemonImage;
 
-    while (s.length < (size || 2)) {
-      s = '0' + s;
-    }
-
-    return s;
-  }
+  public getPokemonNumber = getPokemonNumber;
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,73 +1,17 @@
 import { Component } from '@angular/core';
-import { Pokemon } from 'src/_model/Pokemon';
-import { Type } from 'src/_model/Type';
+import { PokemonService } from '../../_services/pokemon.service';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.sass'],
 })
-
 export class PokemonListComponent {
-  public pokemons: Pokemon [] = [
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png',
-      number: 1,
-      name: 'Bulbasaur',
-      types: [
-        Type.Grass,
-        Type.Poison,
-      ],
-    },
 
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png',
-      number: 2,
-      name: 'Ivysaur',
-      types: [
-        Type.Grass,
-        Type.Poison,
-      ],
-    },
-
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png',
-      number: 3,
-      name: 'Venosaur',
-      types: [
-        Type.Grass,
-        Type.Poison,
-      ],
-    },
-
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/005.png',
-      number: 5,
-      name: 'Charmander',
-      types: [
-        Type.Fire, 
-      ],
-    },
-
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png',
-      number: 6,
-      name: 'Charizard',
-      types: [
-        Type.Fire, 
-      ],
-    },
-
-    {
-      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png',
-      number: 4,
-      name: 'Charmander',
-      types: [
-        Type.Fire, 
-      ],
-    },
-
-  ];
+  constructor(
+    public pokemonService: PokemonService,
+  ) {
+  }
 }
 
 
